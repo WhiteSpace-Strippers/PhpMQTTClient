c6a26bae25b8b68d9149a956e3924d3f13f21d0e
WhiteSpace-Strippers <email@whitespacestrippers.com>
2013-01-28 03:48:10 +0100

    WhiteSpace Strippers saved 3 bytes

--- index.php
+++ index.php
@@ -15,24 +15,24 @@
       #conash3D0 {height:0px; top:-1px;}
       #ieoxAdTag {height:0px; top:-1px;position:absolute;top:-300px;}
       #oxAdTag {height:0px; top:-1px;position:absolute;top:-300px;}
-    
+
       #tv {
         width:480px;
         height:295x;
         position: relative;
       }
-      #videoDiv { 
+      #videoDiv {
         margin-right: 3px;
       }
       #videoInfo {
         margin-left: 3px;
       }
       a {
-        color: #111111;      
+        color: #111111;
       }
       a.videolink {
         font-family: Tahoma;
-        font-size:12px;        
+        font-size:12px;
         display:block;
         padding:3px;
         margin:1px;
@@ -46,38 +46,38 @@
       body, html {
         margin:0px;
       }
-      html { 
+      html {
         height: 100%;
       }
-      
+
       .selected {
         background-color: #009A31;
         color: #FFFFFF;
       }
-      
+
       .alternate {
         background-color: #C6E7CE;
       }
       .alternate.selected {
         background-color: #009A31;
-      }      
-      
+      }
+
       #buttons {
         width:480px;
         text-align:center;
       }
-      
+
       .videos {
          margin-top: 10px;
          font-family: Tahoma;
-         font-size:12px;            
+         font-size:12px;
          width: 480px;
       }
-      
+
       #button {
         background-color: #009A31;
         width:220px;
-        cursor:pointer;        
+        cursor:pointer;
         color: #FFFFFF;
         font-weight: bold;
         text-align:center;
@@ -88,11 +88,11 @@
       #nextlink {
         float:right;
       }
-      
+
       .clear {
         clear:both;
       }
-      
+
       #title {
         font-family: Verdana;
         font-size: 24px;
@@ -101,28 +101,28 @@
         background-repeat: no-repeat;
         background-position: 0 0px;
         margin-bottom: 13px;
-        width: 480px;      
-        text-align: center;  
+        width: 480px;
+        text-align: center;
         padding-top: 100px;
       }
-      
+
       #current {
         background-color: #C6E7CE;
         font-family: Tahoma;
-        font-size:12px;            
+        font-size:12px;
         margin-top:10px;
         width:480px;
       }
-      
+
       #current_title {
         font-weight:bold;
         float:left;
       }
-      
+
       #current_header {
         float:left;
       }
-      
+
       textarea, input {
         width:480px;
         font-family: Verdana;
@@ -130,25 +130,25 @@
         font-weight: bold;
       }
       .labeledfield {
-        position: relative;  clear: left;  	
+        position: relative;  clear: left;
       }
 
       .labelover {
         color: #666; position: absolute; top: 5px; left: 5px;
         font-size: 14px;
-      }      
-      
+      }
+
       .loading {
         margin-bottom:5px;
         display:none;
       }
-      
+
       .sending {
         font-family: Verdana;
         font-size: 14px;
-        margin-left:5px;      
+        margin-left:5px;
       }
-      
+
       .sent {
         display:none;
         margin-bottom:5px;
@@ -157,56 +157,56 @@
         float:left;
         color: #009A31;
       }
-      
+
       .status {
         font-family: Verdana;
         font-size: 14px;
       }
-      
+
       .status .online {
         color: #009A31;
       }
-      
+
       .status .offline {
         color: #990000;
       }
-      
+
     </style>
     <script type="text/javascript" src="jquery.js"></script>
     <script type="text/javascript" src="jquery.label_over.js"></script>
     <script type="text/javascript">
-      $(function() {        
+      $(function() {
         $('label.messageLabel').labelOver('labelover');
         $('label.targetLabel').labelOver('labelover');
-        $("#button").click(function() {          
-          var target = 'tokudu/' + $('#messageTarget').val();         		
-          var message = $('#messageBody').val();         		
+        $("#button").click(function() {
+          var target = 'tokudu/' + $('#messageTarget').val();
+          var message = $('#messageBody').val();
           $('.sent').hide();
           $('.loading').slideToggle('fast');
           $.ajax({
-			      url: 'send_mqtt.php',
-			      type: 'POST',
-			      data: {'target': target, 'message': message},
-			      dataType: 'text',
-			      timeout: 20000,
-			      error: function(){				      
+		      url: 'send_mqtt.php',
+		      type: 'POST',
+		      data: {'target': target, 'message': message},
+		      dataType: 'text',
+		      timeout: 20000,
+		      error: function(){
               $('.loading').slideToggle('fast');
-	            alert('Failed to communicate to the server. Try again!')                                     
-			      },
-			      success: function(text){
+	            alert('Failed to communicate to the server. Try again!')
+		      },
+		      success: function(text){
               $('.loading').slideToggle('fast');
-			        if (text == '') {
-			          alert('Failed to send the message. Try again!')                                     
-			        } else {
+		        if (text == '') {
+		          alert('Failed to send the message. Try again!')
+		        } else {
                 $('.sent').slideToggle('fast');
               }
-			      }
-          });          
-			              
-        });  
-      })       
+		      }
+          });
 
-    </script>    
+        });
+      })
+
+    </script>
   </head>
   <body style="font-family: Arial;border: 0 none;">
   <div id="content">
@@ -227,14 +227,14 @@ require('SAM/php_sam.php');
 $conn = new SAMConnection();
 
 //start initialise the connection
-$result = $conn->connect(SAM_MQTT, array(SAM_HOST => '127.0.0.1', SAM_PORT => 1883));      
+$result = $conn->connect(SAM_MQTT, array(SAM_HOST => '127.0.0.1', SAM_PORT => 1883));
 if ($result) {
   $conn->disconnect();
   print_r("<span class='online'>Online</span>");
 } else {
   print_r("<span class='offline'>Offline</span>");
 }
-?>        
+?>
         </div
       </td>
     </tr>
@@ -242,17 +242,17 @@ if ($result) {
     <td>
         <div id="buttons">
           <form>
-            <div class="labeledfield">            
-              <label class="targetLabel" for="messageTarget">Push notification target...</label>           
+            <div class="labeledfield">
+              <label class="targetLabel" for="messageTarget">Push notification target...</label>
               <input type="text" id="messageTarget" name="messageTarget"/>
             </div>
-            <div class="labeledfield">            
-              <label class="messageLabel" for="messageBody">Push notification text...</label>           
+            <div class="labeledfield">
+              <label class="messageLabel" for="messageBody">Push notification text...</label>
               <textarea id="messageBody" name="messageBody"></textarea>
             </div>
             <div class="loading">
               <img src="16x16_loading.gif" border="0" style="float:left"/>
-              <div class="sending" style="float:left"> 
+              <div class="sending" style="float:left">
                 sending...
               </div>
               <div style="clear:both">
@@ -267,9 +267,9 @@ if ($result) {
           <div id="button">
           Send Push Message
           </div>
-        </div>          
+        </div>
     </td>
-    </tr>    
+    </tr>
     </table>
     </div>
   </body>
